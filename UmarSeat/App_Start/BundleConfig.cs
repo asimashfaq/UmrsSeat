5372f8178ad2023ea4e89dd02546fc7771b5a040
Asim Ashfaq <asimizb4@gmail.com>
2015-09-23 03:50:42 -0700

    Excel file imporrt and previous files

--- UmarSeat/App_Start/BundleConfig.cs
+++ UmarSeat/App_Start/BundleConfig.cs
@@ -31,6 +31,14 @@ namespace UmarSeat
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dropzonescripts").Include(
+                     "~/Scripts/dropzone/dropzone.js"));
+
+            bundles.Add(new StyleBundle("~/Content/dropzonescss").Include(
+                     "~/Scripts/dropzone/css/basic.css",
+                     "~/Scripts/dropzone/css/dropzone.css"));
+
         }
     }
 }
---
+++ UmarSeat/Controllers/ExcelController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UmarSeat.Controllers
+{
+    public class ExcelController : Controller
+    {
+        // GET: Excel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ImportBooking()
+        {
+            return View();
+        }
+
+        public ActionResult SaveUploadedFile()
+        {
+            bool isSavedSuccessfully = true;
+            string fName = "";
+            try
+            {
+                foreach (string fileName in Request.Files)
+                {
+                    HttpPostedFileBase file = Request.Files[fileName];
+                    //Save file content goes here
+                    fName = file.FileName;
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        string Subscription = Session["idSubscription"].ToString();
+                        var originalDirectory = new DirectoryInfo(string.Format("{0}\\imports\\{1}", Server.MapPath(@"\"),Subscription));
+
+                        string pathString = System.IO.Path.Combine(originalDirectory.ToString(), "bookingimport");
+
+                        var fileName1 = Path.GetFileName(file.FileName);
+
+                        bool isExists = System.IO.Directory.Exists(pathString);
+
+                        if (!isExists)
+                            System.IO.Directory.CreateDirectory(pathString);
+
+                        var path = string.Format("{0}\\{1}", pathString, file.FileName);
+                        file.SaveAs(path);
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                isSavedSuccessfully = false;
+            }
+
+
+            if (isSavedSuccessfully)
+            {
+                return Json(new { Message = fName });
+            }
+            else
+            {
+                return Json(new { Message = "Error in saving file" });
+            }
+        }
+    }
+}
