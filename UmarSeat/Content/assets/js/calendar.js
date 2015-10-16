40f44333531689dbb7e506f869921fd13f8b3648
Asim Ashfaq <asimizb4@gmail.com>
2015-10-15 23:05:23 -0700

    Avaliable Stock Changed 0.2

--- UmarSeat/Content/assets/js/calendar.js
+++ UmarSeat/Content/assets/js/calendar.js
@@ -114,16 +114,40 @@ function generateReport(startDate, enddate)
 
                         var oldval = $("#date" + dt + " ." + ob + "").text();
                         var ts = Number(oldval)+ Number(pnr.TotalSeats);
-                        $("#date" + dt + " ." + ob + "").html("<td>" + ts + "</td>");
+                        $("#date" + dt + " ." + ob + "").html("<td class='popup" + dt + "'>" + ts + "</td>");
                         
-                        var predate = 1, currentdate = 2, nextdate = 3;
-                        for (var i = 1 ; i < dt ; i++) {
-                            oldval = $("#date" + i + " ." + ob + "").text();
-                            ts = Number(oldval) + Number(pnr.TotalSeats);
-                            $("#date" + i + " ." + ob + "").html("<td>" + ts + "</td>");
-                            $("#popups .popup"+i+" .content").append("<p>" + pnr.pnrNumber + "</p>");
-                        }
+                       
                         $("#popups .popup" + dt + " .content").append("<p>" + pnr.pnrNumber + "</p>");
+
+                        $("#date" + dt + " ." + ob + "").popover({
+                            html: true,
+                            trigger: "hover",
+                            container: 'body',
+                            placement: function (context, source) {
+                                var position = $(source).position();
+
+                                if (position.left > 515) {
+                                    return "left";
+                                }
+
+                                if (position.left < 515) {
+                                    return "right";
+                                }
+
+                                if (position.top < 110) {
+                                    return "bottom";
+                                }
+
+                                return "top";
+                            },
+                            title: function () {
+                                return $("#popups .popup" + dt + "").find('.head').html();
+                            },
+                            content: function () {
+                                var classname = $(this).find('td')[0].className;
+                                return $("#popups ." + classname + "").find('.content').html();
+                            }
+                        });
                     }
 
 
@@ -141,74 +165,32 @@ function generateReport(startDate, enddate)
                                 var sales = 0;
                                
                                 var oldval = $("#date" + moment(pnrSalesInfo[saleindex].createAt).format('D') + " ." + ob + "").text();
-                                /*if(oldval != "-"  )
-                                {
-                                    
-                                    sales = Number(oldval) +  Number(pnrSalesInfo[saleindex].noOfSeats);
-                                }else
-                                {
-                                        sales = pnrSalesInfo[saleindex].noOfSeats;
-                                }*/
-
+                               
 
                                 var date = moment(pnr.outBoundDate).format('D');
-                                //	console.log("---------------------------------------------------------------");
-                                //	console.log(ob);
-                                //	console.log(pnrSalesInfo[saleindex].createAt);
-                                //	console.log("Seats "+Number(pnrSalesInfo[saleindex].noOfSeats));
+                              
 
 
                                 var totaldays = $("td[id^=date]").length;
-                                var predate = 1, currentdate = 2, nextdate = 3;
-                                for (var i = 1 ; i <= date ; i++) {
-                                 
-                                    if (i >= 2) {
-                                        predate = i - 1;
-                                        currentdate = i;
-                                        if (i < totaldays)
-                                            nextdate = Number(i) + 1;
-                                    }
-
-                                    var ptempSeats = Number($("#date" + predate + " ." + ob + "").text());
-                                    var ctempSeats = Number($("#date" + currentdate + " ." + ob + "").text());
-                                    var ntempSeats = Number($("#date" + nextdate + " ." + ob + "").text());
-                                    	//	console.log("prev :"+ptempSeats+ " Date: "+predate);
-                                    	//	console.log("current: "+ctempSeats+" Date: "+currentdate);
-                                    	//	console.log("next: "+ntempSeats+" Date: "+nexdate);
-                                    //if (ctempSeats != 0)
-                                    {
-                                        var isSaleExits = $("#date" + i + " ." + ob + " .sales");
+                               
+                                    
+                                        var isSaleExits = $("#date" + date + " ." + ob + " .sales");
                                        
-                                        if (ctempSeats >= ptempSeats && ctempSeats >= ntempSeats) {
-                                            var tempSeats = $("#date" + i + " ." + ob + "").text();
+                                       
+                                            var tempSeats = $("#date" + date + " ." + ob + "").text();
                                             sales = Number(tempSeats) - Number(pnrSalesInfo[saleindex].noOfSeats);
 
                                            
-                                            if (i == date)
-                                                $("#date" + i + " ." + ob + "").html("<td class='popup" + i + "'>" + sales + "<span class='sales1'></span></td>");
-                                            else if (isSaleExits.length ==1)
-                                                $("#date" + i + " ." + ob + "").html("<td  class='popup" + i + "'>" + sales + "<span class='sales1'></span></td>");
-                                            else
-                                            {
-                                                $("#date" + i + " ." + ob + "").html("<td  class='popup" + i + "'>" + sales + "</span></td>");
+                                           
+                                            $("#date" + date + " ." + ob + "").html("<td  class='popup" + date + "'>" + sales + "</span></td>");
                                                
-                                             }
+                                             
 
-                                           
-                                        } else if (ctempSeats >= ntempSeats) {
-                                            var tempSeats = $("#date" + i + " ." + ob + "").text();
-                                            sales = Number(tempSeats) - Number(pnrSalesInfo[saleindex].noOfSeats);
-                                            
-                                           
-                                            if(i== date)
-                                                $("#date" + i + " ." + ob + "").html("<td class='popup" + i + "'>" + sales + "<span class='sales1'></span></td>");
-                                            else if (isSaleExits.length ==1)
-                                                $("#date" + i + " ." + ob + "").html("<td class='popup" + i + "'>" + sales + "<span class='sales1'></span></td>");
-                                            else
-                                                $("#date" + i + " ." + ob + "").html("<td class='popup"+i+"'>" + sales + "</span></td>");
+                                         
 
-                                        }
-                                        $("#date" + i + " ." + ob + "").popover({
+
+
+                                            $("#date" + date + " ." + ob + "").popover({
                                             html: true,
                                             trigger: "hover",
                                             container: 'body' ,
@@ -230,17 +212,17 @@ function generateReport(startDate, enddate)
                                                 return "top";
                                             },
                                             title: function () {
-                                                return $("#popups .popup"+i+"").find('.head').html();
+                                                return $("#popups .popup"+date+"").find('.head').html();
                                             },
                                             content: function () {
                                                 var classname = $(this).find('td')[0].className;
                                                 return $("#popups ." + classname + "").find('.content').html();
                                             }
                                         });
-                                    }
+                                    
                                                    
                                     		
-                                }
+                                
                             }
 
                         }
